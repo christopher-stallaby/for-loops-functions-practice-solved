750b76ea26739de981a2146ffec616b9a5e75040
OceanLion <doubter-carbons0g@icloud.com>
2025-09-02 21:34:25 -0500

    all exercises complete

--- exercises/e16.js
+++ exercises/e16.js
@@ -7,8 +7,18 @@
 
 export function flatArrays(array) {
   // Your code goes here...
-
-}
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    if(Array.isArray(array[i])) {
+      for (let j = 0; j < array[i].length; j++) {
+        result.push(array[i][j]);
+      };
+    } else {
+      result.push(array[i]);
+    };
+  };
+  return result;
+};
 
 
 
--- exercises/e20.js
+++ exercises/e20.js
@@ -7,11 +7,32 @@
 
 export function separateNamesWithAFromRest(array) {
   // Your code goes here...
-
-}
+  let result = [];
+  let namesWithA = [];
+  let otherNames = [];
+  for (let i = 0; i < array.length; i++) {
+    let name = array[i];
+    let hasA = false;
+    for (let j = 0; j < name.length; j++) {
+      if (name[j].toLowerCase() === 'a') {
+        hasA = true;
+        break;
+      }
+    }
+   if (hasA) {
+    namesWithA.push(name);
+   } else {
+    otherNames.push(name);
+   }
+  }
+  result.push(namesWithA);
+  result.push(otherNames);
+  return result;
+};
 
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-20"
 // If the test has all tests passed, switch to the next exercise file
 // If any of the tests fails, refactor the code and run the test command after you've fixed the function
+console.log(separateNamesWithAFromRest(["jon", "alice", "andrew", "mark", "jimmy"]));
